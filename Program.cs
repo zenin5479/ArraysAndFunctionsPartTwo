b4e398aab80528c337e71a6a041e205d935a17cf
zenin5479 <zenin5479@gmail.com>
2025-10-19 11:27:20 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArraysAndFunctionsPartTwo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
